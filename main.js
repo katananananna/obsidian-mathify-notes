56ab7eca030ea4780df577e46030cda8efe8f999
Katana <haydnkatana@gmail.com>
2026-09-20 13:53:27 +0800

    Replace stub main.js with 1.6.1 plugin code

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-PLACEHOLDER_USE_LOCAL
+const { Plugin, PluginSettingTab, Setting, Notice } = require('obsidian');
+
+const DEFAULT_SETTINGS = {
+	autoConvert: true,
+	convertChemistry: true,
+	convertEnglishLogicWords: false
+};
+
+module.exports = class MathShorthandPlugin extends Plugin {
+	async onload() {
+		await this.loadSettings();
+	}
+	async loadSettings() { this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData()); }
+	async saveSettings() { await this.saveData(this.settings); }
+};
